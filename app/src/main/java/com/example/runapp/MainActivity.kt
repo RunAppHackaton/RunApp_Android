d8dc362979cf05f49e4a87a0c2f463112b0c4214
EJ-Snowden <shulgindenis2@gmail.com>
2023-11-23 16:35:33 +0100

    RA-167

--- app/src/main/java/com/example/runapp/MainActivity.kt
+++ app/src/main/java/com/example/runapp/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private var myLatitude: Double = 0.0
     private var myLongitude: Double = 0.0
-
+    private var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,6 +61,20 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 else -> false
             }
         }
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            doubleBackToExitPressedOnce = false
+        }, 2000)
     }
 
     override fun onMapReady(map: GoogleMap) {
--- app/src/main/java/com/example/runapp/RunningActivity.kt
+++ app/src/main/java/com/example/runapp/RunningActivity.kt
@@ -1,14 +1,44 @@
 package com.example.runapp
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class RunningActivity : AppCompatActivity() {
+class RunningActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var googleMap: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var myLatitude: Double = 0.0
+    private var myLongitude: Double = 0.0
+    private var doubleBackToExitPressedOnce = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_running)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationViewRunning)
         bottomNavigationView.selectedItemId = R.id.home
@@ -27,6 +57,97 @@ class RunningActivity : AppCompatActivity() {
                     true
                 }
                 else -> false
+            }
+        }
+    }
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            doubleBackToExitPressedOnce = false
+        }, 2000)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        enableMyLocation()
+
+        // Load custom map style
+        try {
+            val success = googleMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    this, R.raw.map_style
+                )
+            )
+
+            if (!success) {
+                Log.e("MapActivity", "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e("MapActivity", "Can't find style. Error: ", e)
+        }
+    }
+
+    private fun enableMyLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            googleMap.isMyLocationEnabled = true
+            googleMap.setOnMyLocationButtonClickListener {
+                // Handle the my location button click, if needed.
+                true
+            }
+
+            // Get the last known location and update the UI
+            fusedLocationProviderClient.lastLocation.addOnCompleteListener(this) { task ->
+                val location: Location? = task.result
+                if (location != null) {
+                    myLatitude = location.latitude
+                    myLongitude = location.longitude
+
+                    // Move the camera to the user's location
+                    val userLocation = LatLng(myLatitude, myLongitude)
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
+                }
+            }
+        } else {
+            // Permissions are not granted, request them.
+            requestPermissions(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ), PERMISSION_REQUEST_ACCESS_LOCATION
+            )
+        }
+    }
+
+    companion object {
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 1001
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISSION_REQUEST_ACCESS_LOCATION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableMyLocation()
+            } else {
+                Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show()
             }
         }
     }
