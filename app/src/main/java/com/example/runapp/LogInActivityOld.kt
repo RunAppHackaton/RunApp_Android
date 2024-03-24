2a68df8e1faa4c397e591215c17856edff7b9729
EJ-Snowden <shulgindenis2@gmail.com>
2024-03-24 14:05:43 +0100

    RA-170 Login design page

---
+++ app/src/main/java/com/example/runapp/LogInActivityOld.kt
@@ -0,0 +1,50 @@
+package com.example.runapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_log_in_old.*
+class LogInActivityOld : AppCompatActivity() {
+
+    private var doubleBackToExitPressedOnce = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in_old)
+
+        logInButton.setOnClickListener {
+            val url = "url"
+
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+
+            startActivity(intent)
+        }
+        regButton.setOnClickListener {
+            val url = "url"
+
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+
+            startActivity(intent)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            doubleBackToExitPressedOnce = false
+        }, 2000)
+    }
+}
